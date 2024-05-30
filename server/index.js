329d896bbdeeda2b0e15ffc8a295a6ff0fed09d9
BHAVDIP BHUKAN <128157200+bhukanbhavdip@users.noreply.github.com>
2024-05-30 15:06:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const bodyParser=require('body-parser')
 const app = express();
 const PORT = 8000;
 
-app.use(cors())
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+))
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended: true}))
